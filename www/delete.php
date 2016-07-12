12ed8add101e1c14403a09c02c17208925d69268
parson_chen <parson_chen@debian>
2016-07-12 03:49:19 -0400

    修正connect.php多於空格，修正do_alter多於空格

--- www/delete.php
+++ www/delete.php
@@ -1,10 +1,10 @@
-<?php 
+<?php
 session_start();
 if ($_SESSION['v']!="yes") {
     header("location:login.php");
 }
 require("connect.php");
-if (isset($_GET['id'])) {    
+if (isset($_GET['id'])) {
     $id=$_GET['id'];
 }
 $query=mysql_query("DELETE FROM message WHERE id = '$id'");
