14445ec289c9fc41185ee69ae32d1cc910b91a95
parson_chen <parson_chen@debian>
2016-07-12 03:42:53 -0400

    再次修正admin.php的html格式，修正alter多於空格、html格式

--- www/admin.php
+++ www/admin.php
@@ -8,62 +8,62 @@ $query=mysql_query('SELECT * FROM message ORDER BY id desc')
 
 ?>
 <html>
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>管理員頁面</title>
-</head>
-<body>
-<div>
-    <ul>
-        <a href="message_index.php">Home</a>
-    </ul>
-    <ul>
-        <a href="login.php">Log In</a>
-    </ul>
-    <ul>
-        <a href="message_index.php">Log out</a>
-    </ul>
-</div>
-<div>
-<h3>管理員頁面</h3>
-</div>
-<p>&nbsp;</p>
-<p>&nbsp;</p>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <title>管理員頁面</title>
+    </head>
+    <body>
+        <div>
+            <ul>
+                <a href="message_index.php">Home</a>
+            </ul>
+            <ul>
+                <a href="login.php">Log In</a>
+            </ul>
+            <ul>
+                <a href="message_index.php">Log out</a>
+            </ul>
+        </div>
+        <div>
+            <h3>管理員頁面</h3>
+        </div>
+            <p>&nbsp;</p>
+            <p>&nbsp;</p>
 <?php
 for ($i = 1; $i <= mysql_num_rows($query); $i++) {
     $message=mysql_fetch_assoc($query);
 
 ?>
-<div>
-    <table align="center">
-        <tr>
-            <td><?php echo $message['title']?></td>
-        </tr>
-        <tr>
-            <td width="25%">暱稱</td>
-            <td width="75%"><?php echo $message['name']?></td>
-        </tr>
-        <tr>
-            <td>信箱</td>
-            <td><?php echo $message['email']?></td>
-        </tr>
-        <tr>
-            <td>性別</td>
-            <td><?php echo $message['gender']?></td>
-        </tr>
-        <tr>
-            <td>留言內容</td>
-            <td><?php echo $message['content']?></td>
-        </tr>
-        <tr>
-            <td><a href="delete.php?id=<?php echo $message['id']?>"> 刪除</a></td>
-            <td><a href="alter.php?id=<?php echo $message['id']?>"> 修改</a></td>
-        </tr>
-    </table>
-</div>
+        <div>
+            <table align="center">
+                <tr>
+                    <td><?php echo $message['title']?></td>
+                </tr>
+                <tr>
+                    <td width="25%">暱稱</td>
+                    <td width="75%"><?php echo $message['name']?></td>
+                </tr>
+                <tr>
+                    <td>信箱</td>
+                    <td><?php echo $message['email']?></td>
+                </tr>
+                <tr>
+                    <td>性別</td>
+                    <td><?php echo $message['gender']?></td>
+                </tr>
+                <tr>
+                    <td>留言內容</td>
+                    <td><?php echo $message['content']?></td>
+                </tr>
+                <tr>
+                    <td><a href="delete.php?id=<?php echo $message['id']?>"> 刪除</a></td>
+                    <td><a href="alter.php?id=<?php echo $message['id']?>"> 修改</a></td>
+                </tr>
+            </table>
+        </div>
 <?php
 }
 
 ?>
-</body>
+    </body>
 </html>
--- www/alter.php
+++ www/alter.php
@@ -15,25 +15,24 @@ for ($i = 1; $i <= mysql_num_rows($query); $i++) {
     </head>
     <body>
         <form id="form" name="form" method="post" action="do_alter.php">
-           
-           <input type="hidden" name="writeid" id="writeid" value="<?php echo $message['id']?>" readonly/><br>
-           <label>標題:</label>
-           <input type="text" name="writetitle" id="writetitle" value="<?php echo $message['title']?>"/><br>
-           <label>暱稱:</label>
-           <input type="text" name="writename" id="writename" value="<?php echo $message['name']?>"/><br>
-           <label>E-mail:</label>
-           <input type="text" name="writeemail" id="writeemail" value="<?php echo $message['email']?>"/><br>
-           <label>性別:</label>           
-           <input type="radio" name="writegender" id="radio" value="男" <?php if($message['gender'] =="男") echo 'checked';?> />男                      
-           <input type="radio" name="writegender" id="radio2" value="女" <?php if($message['gender'] =="女") echo 'checked';?> />女<br>  
-           <label>內容:</label>
-           <input type="text"  name="writecontent" id="writecontent" rows="5" value="<?php echo $message['content']?>"/><br> 
+            <input type="hidden" name="writeid" id="writeid" value="<?php echo $message['id']?>" readonly/><br>
+            <label>標題:</label>
+            <input type="text" name="writetitle" id="writetitle" value="<?php echo $message['title']?>"/><br>
+            <label>暱稱:</label>
+            <input type="text" name="writename" id="writename" value="<?php echo $message['name']?>"/><br>
+            <label>E-mail:</label>
+            <input type="text" name="writeemail" id="writeemail" value="<?php echo $message['email']?>"/><br>
+            <label>性別:</label>
+            <input type="radio" name="writegender" id="radio" value="男" <?php if($message['gender'] =="男") echo 'checked';?> />男
+            <input type="radio" name="writegender" id="radio2" value="女" <?php if($message['gender'] =="女") echo 'checked';?> />女<br>
+            <label>內容:</label>
+            <input type="text"  name="writecontent" id="writecontent" rows="5" value="<?php echo $message['content']?>"/><br>
             <input type="submit" name="button" id="button" value="確認修改"/>
             <a href="admin.php"><button type="button">不修改了</button></a>
         </form>
     </body>
 </html>
-<?php 
+<?php
 }
 
 ?>
