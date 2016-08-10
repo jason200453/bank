17f40118d024d5533a807671e1adbe9143aab381
parson_chen <parson_chen@debian>
2016-08-09 23:02:06 -0400

    新增SQL語法MessagerRepository.php

---
+++ symfony/src/AppBundle/Entity/MessagerRepository.php
@@ -0,0 +1,21 @@
+<?php
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\EntityRepository;
+
+class MessagerRepository extends EntityRepository
+{
+    public function checkMessager($name, $email, $phone)
+    {
+        return $this->getEntityManager()
+            ->createQueryBuilder()
+            ->select('e')
+            ->from('AppBundle:Messager', 'e')
+            ->where('e.name = :checkname AND e.email = :checkemail AND e.phone = :checkphone')
+            ->setParameter('checkname', $name)
+            ->setParameter('checkemail', $email)
+            ->setParameter('checkphone', $phone)
+            ->getQuery()
+            ->getResult();
+    }
+}
