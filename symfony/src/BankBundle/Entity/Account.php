05f999ff5311bb9f0259ed030c733b49a5c33c29
parson_chen <parson_chen@debian>
2016-08-15 03:07:54 -0400

    新增account帳戶與entry明細 兩個Table

---
+++ symfony/src/BankBundle/Entity/Account.php
@@ -0,0 +1,195 @@
+<?php
+
+namespace BankBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Doctrine\Common\Collections\ArrayCollection;
+
+/**
+ * 帳戶
+ *
+ * @ORM\Entity
+ * @ORM\Table(name = "account")
+ */
+class Account
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type = "integer")
+     * @ORM\GeneratedValue
+     */
+    private $id = null;
+
+    /**
+     * 帳號
+     *
+     * @ORM\Column(type = "string", name = "account", length = 20, nullable = false)
+     */
+    private $account = null;
+
+    /**
+     * 姓名
+     *
+     * @ORM\Column(type = "string", name = "name", length = 10, nullable = false)
+     */
+    private $name = null;
+
+    /**
+     * 電話
+     *
+     * @ORM\Column(type = "string", name = "phone", length = 10, nullable = false)
+     */
+    private $phone = null;
+
+    /**
+     * 帳戶餘額
+     *
+     * @ORM\Column(type = "integer", name = "balance", length = 100, nullable = false)
+     */
+    private $balance = null;
+
+    /**
+     * @ORM\OneToMany(targetEntity = "Entry", mappedBy = "account")
+     */
+    private $entry;
+
+    public function __construct()
+    {
+        $this->entry = new ArrayCollection();
+    }
+
+    /**
+     * 取得id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * 創建帳號
+     *
+     * @param string $account
+     * @return Account
+     */
+    public function setAccount($account)
+    {
+        $this->account = $account;
+
+        return $this;
+    }
+
+    /**
+     * 取得帳號
+     *
+     * @return string
+     */
+    public function getAccount()
+    {
+        return $this->account;
+    }
+
+    /**
+     * 創建姓名
+     *
+     * @param string $name
+     * @return Account
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * 取得姓名
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * 創建電話
+     *
+     * @param string $phone
+     * @return Account
+     */
+    public function setPhone($phone)
+    {
+        $this->phone = $phone;
+
+        return $this;
+    }
+
+    /**
+     * 取得電話
+     *
+     * @return string
+     */
+    public function getPhone()
+    {
+        return $this->phone;
+    }
+
+    /**
+     * 創建帳戶餘額
+     *
+     * @param integer $balance
+     * @return Account
+     */
+    public function setBalance($balance)
+    {
+        $this->balance = $balance;
+
+        return $this;
+    }
+
+    /**
+     * 取得帳戶餘額
+     *
+     * @return integer
+     */
+    public function getBalance()
+    {
+        return $this->balance;
+    }
+
+    /**
+     * 新增明細
+     *
+     * @param \BankBundle\Entity\Entry $entry
+     * @return Account
+     */
+    public function addEntry(\BankBundle\Entity\Entry $entry)
+    {
+        $this->entry[] = $entry;
+
+        return $this;
+    }
+
+    /**
+     * 移除明細
+     *
+     * @param \BankBundle\Entity\Entry $entry
+     */
+    public function removeEntry(\BankBundle\Entity\Entry $entry)
+    {
+        $this->entry->removeElement($entry);
+    }
+
+    /**
+     * 取得明細
+     *
+     * @return \Doctrine\Common\Collections\Collection
+     */
+    public function getEntry()
+    {
+        return $this->entry;
+    }
+}
---
+++ symfony/src/BankBundle/Entity/Entry.php
@@ -0,0 +1,150 @@
+<?php
+
+namespace BankBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * 明細
+ *
+ * @ORM\Entity
+ * @ORM\Table(name = "entry")
+ */
+class Entry
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type = "integer")
+     * @ORM\GeneratedValue
+     */
+    private $id = null;
+
+    /**
+     * 交易金額
+     *
+     * @ORM\Column(type = "integer", name = "amount", length = 20, nullable = false)
+     */
+    private $amount = null;
+
+    /**
+     * 交易日期時間
+     *
+     * @ORM\Column(type = "datetimetz", name = "datetime", nullable = false)
+     */
+    private $datetime = null;
+
+    /**
+     * 此筆交易後之餘額
+     *
+     * @ORM\Column(type = "integer", name = "balance", length = 100, nullable = false)
+     */
+    private $balance = null;
+
+    /**
+     * @ORM\ManyToOne(targetEntity = "Account", inversedBy = "entry")
+     * @ORM\JoinColumn(name = "account_id", referencedColumnName = "id", onDelete = "CASCADE")
+     */
+    private $account = null;
+
+    /**
+     * 取得id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * 創建交易金額
+     *
+     * @param integer $amount
+     * @return Entry
+     */
+    public function setAmount($amount)
+    {
+        $this->amount = $amount;
+
+        return $this;
+    }
+
+    /**
+     * 取得交易金額
+     *
+     * @return integer
+     */
+    public function getAmount()
+    {
+        return $this->amount;
+    }
+
+    /**
+     * 創建交易時間
+     *
+     * @param \DateTime $datetime
+     * @return Entry
+     */
+    public function setDatetime($datetime)
+    {
+        $this->datetime = $datetime;
+
+        return $this;
+    }
+
+    /**
+     * 取得交易時間
+     *
+     * @return \DateTime
+     */
+    public function getDatetime()
+    {
+        return $this->datetime;
+    }
+
+    /**
+     * 創建此筆交易之後之餘額
+     *
+     * @param integer $balance
+     * @return Entry
+     */
+    public function setBalance($balance)
+    {
+        $this->balance = $balance;
+
+        return $this;
+    }
+
+    /**
+     * 取得此筆交易之後之餘額
+     *
+     * @return integer
+     */
+    public function getBalance()
+    {
+        return $this->balance;
+    }
+
+    /**
+     * 創建帳戶
+     *
+     * @param \BankBundle\Entity\Account $account
+     * @return Entry
+     */
+    public function setAccount(\BankBundle\Entity\Account $account = null)
+    {
+        $this->account = $account;
+
+        return $this;
+    }
+
+    /**
+     * 取得帳戶
+     *
+     * @return \BankBundle\Entity\Account
+     */
+    public function getAccount()
+    {
+        return $this->account;
+    }
+}
