35c8fae0700428ccbe2cdf6bf71cb1e4085672d4
parson_chen <parson_chen@debian>
2016-09-08 00:19:57 -0400

    修改Command寫法

--- symfony/src/BankBundle/Command/TransmissionCommand.php
+++ symfony/src/BankBundle/Command/TransmissionCommand.php
@@ -1,9 +1,9 @@
 <?php
+
 namespace BankBundle\Command;
 
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 
 class TransmissionCommand extends ContainerAwareCommand
@@ -13,23 +13,33 @@ class TransmissionCommand extends ContainerAwareCommand
         $this->setName('transmission');
         $this->setDescription('Transmission.');
         $this->setHelp("This command allows you to transmission balance.");
-        $this->addArgument('accountId', InputArgument::REQUIRED, 'The accountId.');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $em = $this->getContainer()->get('doctrine')->getManager();
 
-        $output->writeln('AccountId: '.$input->getArgument('accountId'));
         $redis = $this->getContainer()->get('snc_redis.default');
 
-        $balance = $redis->hget($input->getArgument('accountId'), 'balance');
-        //$version = $redis->hget($input->getArgument('accountId'), 'version');
+        try {
+            $accounts = $redis->smembers('account');
 
-        $account = $em->find('BankBundle:Account', $input->getArgument('accountId'));
-        $account->setBalance($balance);
-        $em->flush();
+            for ($i = 0; $i < count($accounts); $i++) {
+                $version = $redis->hget($accounts[$i], 'version');
+                $balance = $redis->hget($accounts[$i], 'balance');
 
-        $output->writeln('Success');
+                $account = $em->find('BankBundle:Account', $accounts[$i]);
+
+                $account->setBalance($balance);
+                $account->setVersion($version);
+                $em->flush();
+
+                $redis->srem('account', $accounts[$i]);
+            }
+
+        } catch (Exception $e) {
+
+            throw $e;
+        }
     }
 }
